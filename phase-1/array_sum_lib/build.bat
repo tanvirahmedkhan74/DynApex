ea5df546a39c874b99cd7241c7c1bc4245d55813
tanvirahmedkhan74 <tanvirahmedkhan74@gmail.com>
2024-10-24 23:32:31 +0600

    .dll file building success | .lib not resolved

--- phase-1/array_sum_lib/build.bat
+++ phase-1/array_sum_lib/build.bat
@@ -2,3 +2,4 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 set INCLUDE=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.41.34120\include;C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\ucrt;C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\shared;C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\um;C:\Program Files (x86)\Windows Kits\10\Include\10.0.22621.0\winrt
 make
+make test
